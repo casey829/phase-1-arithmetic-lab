23983c6e709552cbe5d0351b69175860d42bdf2e
casey <caseymwangi31@gmail.com>
2024-03-20 14:54:10 +0300

    added content

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+const casey = function(num1, num2){
+    return num1 * num2
+};
+
+let multiply = casey(31, 2);
+
+
+const random = Math.ceil(Math.random ()*6) + 1;
+
+let num3 = 10;
+let num4 = 6;
+
+const mod =  num3 % num4;
+
+const max = Math.max(5,10,15,20);
